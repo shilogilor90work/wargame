4cba087b484540a1267607ff3888700678659ba6
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-05-24 19:01:22 +0300

    undo

--- Soldier.hpp
+++ Soldier.hpp
@@ -12,6 +12,7 @@ public:
   int original_health;
   Soldier(uint t);
   virtual void attack(Board* board, int row, int column)=0;
+  uint get_team(){return team;}
   virtual void FootCommanderSaysShoot(Board* board, int row, int column){}
   virtual void SniperCommanderSaysShoot(Board* board, int row, int column){}
   virtual void ParamedicCommanderSaysShoot(Board* board, int row, int column){}
